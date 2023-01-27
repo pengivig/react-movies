ca32ac965940ea79036dc68cf55155535e5be481
pengivig <pengivig@icloud.com>
2023-01-27 15:35:10 +0300

    link fixed

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
             <div className="nav-wrapper">
                <a href="#" className="brand-logo">Movies</a>
                <ul id="nav-mobile" className="right hide-on-med-and-down">
-                  <li><a href="!#">Repo</a></li>
+                  <li><a href="https://github.com/pengivig/react-movies">Repo</a></li>
                </ul>
             </div>
          </nav>
